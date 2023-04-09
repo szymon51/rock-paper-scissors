538edb4192e0b6c2df5c7ea6fa5c928554865378
Szymon51 <120176777+szymon51@users.noreply.github.com>
2023-04-09 15:07:42 +0100

    Corrected the logic in playRound to match that of paper rock and scissors

--- script.js
+++ script.js
@@ -10,12 +10,37 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    let userChoice = prompt("Hello!", "scissors");
-    userChoice = userChoice.toLowerCase();
-    if(userChoice === computerSelection) console.log("you won!");
-    else console.log("you lost"); 
+    let message = ""; 
+    console.log(`${playerSelection}, ${computerSelection}`); 
+    if (playerSelection === "scissors" && computerSelection === "paper") {
+        message = `You won! ${playerSelection} beats ${computerSelection}`;
+    }
+    else if (playerSelection === "rock" && computerSelection === "scissors") {
+        message = `You won! ${playerSelection} beats ${computerSelection}`;
+    }
+    else if (playerSelection === "paper" && computerSelection === "rock") {
+        message = `You lost! ${computerSelection} beats ${playerSelection}`;
+    }
+    else if (computerSelection === "scissors" && playerSelection === "paper") {
+        message = `You lost! ${computerSelection} beats ${playerSelection}`;
+    }
+    else if (computerSelection === "rock" && playerSelection === "scissors") {
+        message = `You lost! ${computerSelection} beats ${playerSelection}`;
+    }
+    else if (computerSelection === "paper" && playerSelection === "rock") {
+        message = `You lost! ${computerSelection} beats ${playerSelection}`;
+    }
+    else message = "It is a tie!";
+    return message;
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+    let numberOfGamesPlayed = 0;
+    let timesUserWon = 0;
+    let playerSelection = prompt("Please enter you choice").toLowerCase();
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+}
+
+game();
+
