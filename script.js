e44fbc93a14f13ec58a5a0c1f3a7a28787148e49
Szymon51 <120176777+szymon51@users.noreply.github.com>
2023-04-09 12:04:19 +0100

    Add getComputerChoice()

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function getComputerChoice() {
+    let randomNumber = Math.ceil(Math.random() * 3);
+    let computerChoice;
 
+    if (randomNumber === 1) computerChoice = "rock";
+    else if (randomNumber === 2) computerChoice = "paper";
+    else if (randomNumber === 3) computerChoice = "scissors";
+    else console.log("There was an error generating the computer choice");
+    return computerChoice;
 }
