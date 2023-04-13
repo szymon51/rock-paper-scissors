8e304f2d215617fda1535820d7a12217618574cb
Szymon51 <120176777+szymon51@users.noreply.github.com>
2023-04-13 21:09:08 +0100

    Add UI

--- script.js
+++ script.js
@@ -13,33 +13,71 @@ function playRound(playerSelection, computerSelection) {
     let message = ""; 
     if (playerSelection === "scissors" && computerSelection === "paper") {
         message = `You won! ${playerSelection} beats ${computerSelection}`;
+        numberOfWins++;
     }
     else if (playerSelection === "rock" && computerSelection === "scissors") {
         message = `You won! ${playerSelection} beats ${computerSelection}`;
+        numberOfWins++;
     }
     else if (playerSelection === "paper" && computerSelection === "rock") {
         message = `You won! ${playerSelection} beats ${computerSelection}`;
+        numberOfWins++;
     }
     else if (computerSelection === "scissors" && playerSelection === "paper") {
         message = `You lost! ${computerSelection} beats ${playerSelection}`;
+        numberOfLoses++;
     }
     else if (computerSelection === "rock" && playerSelection === "scissors") {
         message = `You lost! ${computerSelection} beats ${playerSelection}`;
+        numberOfLoses++;
     }
     else if (computerSelection === "paper" && playerSelection === "rock") {
         message = `You lost! ${computerSelection} beats ${playerSelection}`;
+        numberOfLoses++;
     }
     else if (computerSelection === playerSelection) {
-    message = "It is a tie!";
+        message = "It is a tie!";
+        numberOfTies++;
     }
     return message;
 }
 
+function endOfGame(whoWon) {
+    numberOfWins = 0;
+    numberOfLoses = 0;
+    numberOfTies = 0; 
+
+    alert(`${whoWon} won!`);
+}
+
+function updateScore() {
+    const wins = document.querySelector('#wins');
+    const loses = document.querySelector('#loses');
+    const ties = document.querySelector('#ties');
+
+    wins.textContent = `WINS: ${numberOfWins}`;
+    loses.textContent = `LOSES: ${numberOfLoses}`;
+    ties.textContent = `TIES: ${numberOfTies}`;
+    
+    if (numberOfLoses >= 5) endOfGame("Computer"); 
+    else if (numberOfWins >= 5) endOfGame("You"); 
+}
+
+
+
 function game(buttonName) {
     computerSelection = getComputerChoice();
     playerSelection = buttonName;
-    console.log(playRound(playerSelection, computerSelection));
+    
+    result = document.querySelector('#result');
+    result.textContent = (playRound(playerSelection, computerSelection));
+
+    updateScore();
 }
+
+let numberOfWins = 0;
+let numberOfLoses = 0;
+let numberOfTies = 0;
 
 const buttons = document.querySelectorAll('button');
 buttons.forEach(button => {
