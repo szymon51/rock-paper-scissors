83ba3f6c531a6359ba3b3302711046356e297fa6
Szymon51 <120176777+szymon51@users.noreply.github.com>
2023-04-13 19:29:19 +0100

    Add working buttons for rock paper and scissors

--- script.js
+++ script.js
@@ -13,15 +13,12 @@ function playRound(playerSelection, computerSelection) {
     let message = ""; 
     if (playerSelection === "scissors" && computerSelection === "paper") {
         message = `You won! ${playerSelection} beats ${computerSelection}`;
-        timesUserWon++;
     }
     else if (playerSelection === "rock" && computerSelection === "scissors") {
         message = `You won! ${playerSelection} beats ${computerSelection}`;
-        timesUserWon++;
     }
     else if (playerSelection === "paper" && computerSelection === "rock") {
-        message = `You won! ${computerSelection} beats ${playerSelection}`;
-        timesUserWon++;
+        message = `You won! ${playerSelection} beats ${computerSelection}`;
     }
     else if (computerSelection === "scissors" && playerSelection === "paper") {
         message = `You lost! ${computerSelection} beats ${playerSelection}`;
@@ -34,22 +31,19 @@ function playRound(playerSelection, computerSelection) {
     }
     else if (computerSelection === playerSelection) {
     message = "It is a tie!";
-    } else message = "something went wrong!";
-    numberOfGamesPlayed++;
+    }
     return message;
 }
 
-function game() {
-    let playerSelection = prompt("Please enter you choice").toLowerCase();
-    const computerSelection = "rock";
+function game(buttonName) {
+    computerSelection = getComputerChoice();
+    playerSelection = buttonName;
     console.log(playRound(playerSelection, computerSelection));
-    if (timesUserWon >= 3 && numberOfGamesPlayed === 5) alert("You won the whole game");
 }
 
-let numberOfGamesPlayed = 0;
-let timesUserWon = 0;
-
-for (let i = 0; i < 5; i++) {
-    game();
-}
-
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        game(button.id);
+    })
+});
